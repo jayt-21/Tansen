f6ed3dc112511d75b3a6f0f3c689ad68d6a68be3
jayt-21 <jayantthakur405@gmail.com>
2025-07-13 10:11:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+<div className=' bg-amber-500 flex justify-center items-center w-screen h-screen size-64'> hello dinho</div>
+  )
+}
+
+export default App
